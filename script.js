2dde907357a496c3fc8342c5145ac52831dcb013
Faisal25DEC <faisalmd25121999@gmail.com>
2023-11-18 15:09:42 +0530

    portfolio ui updates

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-new Splide("#image-carousel", {
-  rewind: true,
-}).mount();
-new Splide("#image-carousel1", {
-  rewind: true,
-}).mount();
-new Splide("#image-carousel2", {
-  rewind: true,
-}).mount();
-new Splide("#image-carousel3", {
-  rewind: true,
-}).mount();
-new Splide("#image-carousel4", {
-  rewind: true,
-}).mount();
+// new Splide("#image-carousel", {
+//   rewind: true,
+// }).mount();
+// new Splide("#image-carousel1", {
+//   rewind: true,
+// }).mount();
+// new Splide("#image-carousel2", {
+//   rewind: true,
+// }).mount();
+// new Splide("#image-carousel3", {
+//   rewind: true,
+// }).mount();
+// new Splide("#image-carousel4", {
+//   rewind: true,
+// }).mount();
 
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
@@ -25,6 +25,320 @@ navLinksArray.forEach((navLink) => {
   navLink.addEventListener("click", () => {
     navLinks.classList.remove("left");
   });
+});
+
+const projectContainer = document.querySelector(".projects-container");
+
+const items = [
+  {
+    id: 1,
+    title: "Adventour",
+    img: "./assets/adventour-collage-2.png",
+    desc: `
+        A beautifully animated and entirely user-specific website built on
+        <b>React.js and Firebase</b> and many other CSS libraries and framework
+        for design
+      `,
+    techStack: [
+      "./assets/react.svg",
+      "./assets/firebase.svg",
+      "./assets/bootstrap.svg",
+      "./assets/json-server.png",
+    ],
+    live: "https://adventourr-masai.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "BlogsTour.com",
+    img: "./assets/medium-collage-1.png",
+    desc: `
+        A Complete <b>MERN Stack</b> blog website similar to <b>Medium.com</b>
+        which supports CRUD operations and have entirely user specific features
+        like comments, like, unlike, follows etc.
+      `,
+    techStack: [
+      "./assets/react.svg",
+      "./assets/mongo.svg",
+      "./assets/chakra.png",
+      "./assets/nodejs1.png",
+      "./assets/redux.svg",
+    ],
+    live: "https://medium-blog-app-pi.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "Socialms",
+    img: "./assets/twitter-collage-2.png",
+    desc: `A full fledged social media platforms inpired by <b>Twitter</b> which supports messages, create posts edit profiles and users can send images in messages as well`,
+    techStack: [
+      "./assets/next.svg",
+      "./assets/mongo.svg",
+      "./assets/tailwind.svg",
+      "./assets/prisma.svg",
+      "./assets/firebase.svg",
+    ],
+    live: "https://twitter-clone-faisal25dec.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Alcazar Travel Agency",
+    img: "./assets/alcazar-collage-1.png",
+    desc: ` 
+        <b>User Specific Vanilla Javascript E-commerce website</b>, built on
+        HTML, CSS, Javascript, Firebase and JSON-Server`,
+    techStack: [
+      "./assets/html.svg",
+      "./assets/css.svg",
+      "./assets/js.svg",
+      "./assets/json-server.png",
+      "./assets/firebase.svg",
+    ],
+    live: "https://alcazar-masai.netlify.app/pages/product",
+  },
+  {
+    id: 4,
+    title: "Instagram Clone",
+    img: "./assets/instgram-collage-1.png",
+    desc: `
+        A complete working clone of Instagram where users can Create Posts,
+        <b>Update their profiles</b>, and have all features of instagram
+      `,
+    techStack: [
+      "./assets/react.svg",
+      "./assets/nodejs1.png",
+      "./assets/mongo.svg",
+      "./assets/chakra.png",
+      "./assets/redux.svg",
+    ],
+    live: "https://instaclonevi.netlify.app/",
+  },
+];
+
+items.forEach((item, index) => {
+  let allImages = "";
+  item.techStack.forEach((item) => {
+    allImages += `<img src = ${item} title = ${
+      item.split("/")[2].split(".")[0]
+    }/>`;
+  });
+  console.log(item.img);
+  projectContainer.innerHTML += ` <article class="project-card best mix" data-order=${
+    index + 1
+  }>
+  <div class="project-image">
+    <img src = ${item.img} />
+  </div>
+  <div class = "project-text">
+  <h1 class="project-title">${item.title}</h1>
+  <p class="project-description">
+   ${item.desc}
+  </p>
+  <div class="project-link-tech-stack-wrapper">
+    <div class="project-links">
+      <a
+        href="https://github.com/Faisal25DEC/Alcazar-Travel-Agency"
+        target="_blank"
+        class="project-github-link"
+        ><img src="./assets/github.png" alt="" /><span>Github</span></a
+      >
+      <a
+        href=${item.live}
+        target="_blank"
+        class="project-deployed-link"
+        ><img src="./assets/netlify.png" alt="" />Live</a
+      >
+    </div>
+    <div class="tech-stack">
+
+    ${allImages}
+      
+      
+    </div>
+  </div>
+  </div>
+</article>`;
+});
+
+const frontEndItems = [
+  {
+    id: 1,
+    title: "Myntra Clone",
+    img: "./assets/myntra-collage-1.png",
+    desc: "Clone of famous fashion centric website <b>Myntra.com</b> built on <b>HTML, CSS And Javascript</b> with the help of localstorage. ",
+    techStack: [
+      "./assets/html.svg",
+      "./assets/css.svg",
+      "./assets/js.png",
+      "./assets/local-storage.png",
+    ],
+  },
+  {
+    id: 2,
+    title: "SportyVook",
+    img: "./assets/playo-collage-1.png",
+    desc: "Clone of Playo website built as a part of open source project given as an assignment during my tenure at Masai School",
+    techStack: [
+      "./assets/html.svg",
+      "./assets/css.svg",
+      "./assets/js.svg",
+      "./assets/local-storage.png",
+    ],
+  },
+  {
+    id: 3,
+    title: "Alcazar Travel Agency",
+    img: "./assets/alcazar-collage-1.png",
+    desc: ` 
+        <b>User Specific Vanilla Javascript E-commerce website</b>, built on
+        HTML, CSS, Javascript, Firebase and JSON-Server`,
+    techStack: [
+      "./assets/html.svg",
+      "./assets/css.svg",
+      "./assets/js.svg",
+      "./assets/json-server.png",
+      "./assets/firebase.svg",
+    ],
+    live: "https://alcazar-masai.netlify.app/pages/product",
+  },
+];
+
+frontEndItems.forEach((item) => {
+  let allImages = "";
+  item.techStack.forEach((item) => {
+    allImages += `<img src = ${item} title = ${
+      item.split("/")[2].split(".")[0]
+    }/>`;
+  });
+  console.log(item.img);
+  projectContainer.innerHTML += ` <article class="project-card mix front-end " data-order="1">
+  <div class="project-image">
+    <img src = ${item.img} />
+  </div>
+  <div class = "project-text">
+  <h1 class="project-title">${item.title}</h1>
+  <p class="project-description">
+   ${item.desc}
+  </p>
+  <div class="project-link-tech-stack-wrapper">
+    <div class="project-links">
+      <a
+        href="https://github.com/Faisal25DEC/Alcazar-Travel-Agency"
+        target="_blank"
+        class="project-github-link"
+        ><img src="./assets/github.png" alt="" /><span>Github</span></a
+      >
+      <a
+        href=${item.live}
+        target="_blank"
+        class="project-deployed-link"
+        ><img src="./assets/netlify.png" alt="" />Live</a
+      >
+    </div>
+    <div class="tech-stack">
+
+    ${allImages}
+      
+      
+    </div>
+  </div>
+  </div>
+</article>`;
+});
+
+const fullStackItems = [
+  {
+    id: 2,
+    title: "BlogsTour.com",
+    img: "./assets/medium-collage-1.png",
+    desc: `
+        A Complete <b>MERN Stack</b> blog website similar to <b>Medium.com</b>
+        which supports CRUD operations and have entirely user specific features
+        like comments, like, unlike, follows etc.
+      `,
+    techStack: [
+      "./assets/react.svg",
+      "./assets/mongo.svg",
+      "./assets/chakra.png",
+      "./assets/nodejs1.png",
+      "./assets/redux.svg",
+    ],
+    live: "https://medium-blog-app-pi.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Socialms",
+    img: "./assets/twitter-collage-2.png",
+    desc: `A full fledged social media platforms inpired by <b>Twitter</b> which supports messages, create posts edit profiles and users can send images in messages as well`,
+    techStack: [
+      "./assets/next.svg",
+      "./assets/mongo.svg",
+      "./assets/tailwind.svg",
+      "./assets/prisma.svg",
+      "./assets/firebase.svg",
+    ],
+    live: "https://twitter-clone-faisal25dec.vercel.app/",
+  },
+
+  {
+    id: 4,
+    title: "Instagram Clone",
+    img: "./assets/instgram-collage-1.png",
+    desc: `
+        A complete working clone of Instagram where users can Create Posts,
+        <b>Update their profiles</b>, and have all features of instagram
+      `,
+    techStack: [
+      "./assets/react.svg",
+      "./assets/nodejs1.png",
+      "./assets/mongo.svg",
+      "./assets/chakra.png",
+      "./assets/redux.svg",
+    ],
+    live: "https://instaclonevi.netlify.app/",
+  },
+];
+
+fullStackItems.forEach((item) => {
+  let allImages = "";
+  item.techStack.forEach((item) => {
+    allImages += `<img src = ${item} title = ${
+      item.split("/")[2].split(".")[0]
+    }/>`;
+  });
+  console.log(item.img);
+  projectContainer.innerHTML += ` <article class="project-card mix full-stack " data-order="1">
+  <div class="project-image">
+    <img src = ${item.img} />
+  </div>
+  <div class = "project-text">
+  <h1 class="project-title">${item.title}</h1>
+  <p class="project-description">
+   ${item.desc}
+  </p>
+  <div class="project-link-tech-stack-wrapper">
+    <div class="project-links">
+      <a
+        href="https://github.com/Faisal25DEC/Alcazar-Travel-Agency"
+        target="_blank"
+        class="project-github-link"
+        ><img src="./assets/github.png" alt="" /><span>Github</span></a
+      >
+      <a
+        href=${item.live}
+        target="_blank"
+        class="project-deployed-link"
+        ><img src="./assets/netlify.png" alt="" />Live</a
+      >
+    </div>
+    <div class="tech-stack">
+
+    ${allImages}
+      
+      
+    </div>
+  </div>
+  </div>
+</article>`;
 });
 
 //----------------mix it up------------
